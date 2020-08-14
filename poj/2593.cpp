20600a9a6722b8dbf9c9025d843d9e4c88d8acf6
Murphy-OrangeMud <chengzhiyi_2@163.com>
2020-08-14 22:43:25 +0800

    2593

---
+++ poj/2593.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+using namespace std;
+int t;
+int n;
+const long long maxn = 100005;
+long long a[maxn];
+long long bsum[maxn];
+long long esum[maxn];
+long long mesum[maxn];
+int main() {
+    while (cin >> n && n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%lld", &a[i]);
+        }
+        bsum[0] = a[0];
+        for (int i = 1; i < n; i++) {
+            bsum[i] = max(a[i] + bsum[i-1], a[i]);
+        }
+        esum[n-1] = a[n-1];
+        mesum[n-1] = esum[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            esum[i] = max(a[i] + esum[i+1], a[i]);
+            mesum[i] = max(mesum[i+1], esum[i]);
+        }
+        long long maximum = -1000ll * maxn;
+        for (int i = 0; i < n-1; i++) {
+            maximum = max(maximum,  bsum[i] + mesum[i+1]);
+        }
+        printf("%lld\n", maximum);
+    }
+    return 0;
+}
---
+++ poj/README.md
@@ -0,0 +1,3 @@
+The code in this folder are all accepted by poj.  
+The names of the files are the numbers of problems.  
+A place holder.
