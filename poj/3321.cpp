b1b22272e5fff5127a05ae77e5a73eceb8a133ce
Murphy-OrangeMud <chengzhiyi_2@163.com>
2020-08-21 22:19:20 +0800

    segment tree solution for poj 3321 but time limit exceeded due to vector

--- poj/3321.cpp
+++ poj/3321.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+// WA原因：change那个地方忘记设置hasapple为false了
+#include <cstdio>
 #include <vector>
-#include <algorithm>
+#include <cstring>
+#include <iostream>
 using namespace std;
 const int maxn = 100005;
 struct v {
@@ -13,6 +15,7 @@ struct v {
 void change(int i) {
     int cur = i;
     int applechange = vs[i].hasapple? -1: 1;
+    vs[i].hasapple = !vs[i].hasapple;
     while (cur != 0) {
         vs[cur].applecnt += applechange;
         cur = vs[cur].parent;
@@ -23,35 +26,36 @@ int query(int i) {
 }
 int init(int cur, int parent) {
     vs[cur].parent = parent;
+    vs[cur].num = cur;
+    vs[cur].hasapple = true;
     int t = 1;
     for (int i = 0; i < vs[cur].sons.size(); i++) {
         if (vs[cur].sons[i] == parent) continue;
         t += init(vs[cur].sons[i], cur);
     }
-    vs[cur].num = cur;
-    vs[cur].hasapple = true;
     vs[cur].applecnt = t;
     return t;
 }
 int main() {
     int n;
-    cin >> n;
+    scanf("%d", &n);
+    memset(vs, 0, sizeof(vs));
     for (int i = 1; i <= n - 1; i++) {
         int father, son;
-        cin >> father >> son;
+        scanf("%d%d", &father, &son);
         vs[father].sons.push_back(son);
         vs[son].sons.push_back(father);
     }
     init(1, 0);
     int m;
-    cin >> m;
+    scanf("%d", &m);
     for (int i = 0; i < m; i++) {
         char op; int num;
         cin >> op >> num;
         if (op == 'C') {
             change(num);
         } else {
-            cout << query(num) << endl;
+            printf("%d\n", query(num));
         }
     }
     return 0;
