92054fa472ccdf708a0b71eff6753f4781b397fd
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-03-15 22:26:48 +0800

    complete problem

---
+++ codeforces/div2_705/3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        vector<int> mine;
+        vector<int> miner;
+        int n;
+        scanf("%d", &n);
+        for (int i = 0; i < 2*n; i++) {
+            int x, y;
+            scanf("%d%d", &x, &y);
+            if (x==0) miner.push_back(abs(y));
+            if (y==0) mine.push_back(abs(x));
+        }
+        sort(mine.begin(), mine.end());
+        sort(miner.begin(), miner.end());
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += sqrt(mine[i]*mine[i]+miner[i]*miner[i]);
+        }
+        printf("%.9f", sum);
+    }
+    return 0;
+}
