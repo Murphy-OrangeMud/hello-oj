dc0ae49f1a024d2f184f5daa75d8920ba012287b
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-03-18 10:17:41 +0800

    complete problem

--- codeforces/div2_708/3_2.cpp
+++ codeforces/div2_708/3_2.cpp
@@ -1,34 +1,44 @@
-#include<iostream>
-#include<cstdio>
+#include <iostream>
+#include <cstdio>
 using namespace std;
 
-int main() {
+void run3(int n)
+{
+    if (n % 3 == 0)
+    {
+        printf("%d %d %d\n", n / 3, n / 3, n / 3);
+    }
+    else
+    {
+        if (n % 2 == 0)
+        {
+            for (int x = 1; x <= n / 4; x++)
+            {
+                if ((n / 2 - x) % (x * 2) == 0 || (2 * x) % (n / 2 - x) == 0)
+                {
+                    printf("%d %d %d\n", 2 * x, n / 2 - x, n / 2 - x);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            printf("%d %d %d\n", 1, n / 2, n / 2);
+        }
+    }
+}
+
+int main()
+{
     int t;
     scanf("%d", &t);
     while (t--) {
         int n, k;
         scanf("%d%d", &n, &k);
-        if (n % k == 0) {
-            for (int i = 0; i < k; i++) {
-                if (i) printf(" ");
-                printf("%d", n/k);
-            }
-            printf("\n");
+        for (int i = 0; i < k-3; i++) {
+            printf("1 ");
         }
-        else {
-            for (int x = 1; x <= n/2; x++) {
-                if ((n/2-(k-2)*x/2) < 0) break;
-                if ((n/2-(k-2)*x/2)+(n/2-(k-2)*x/2)+(k-2)*x != n) continue;
-                if ((x == 1) || ((n/2-(k-2)*x/2)%x == 0) || (x%(n/2-(k-2)*x/2) == 0)) {
-                    for (int i = 0; i < k-2; i++){
-                        if (i) printf(" ");
-                        printf("%d", x);
-                    }
-                    printf(" %d %d\n", (n/2-(k-2)*x/2), (n/2-(k-2)*x/2));
-                    break;
-                }
-            }
-        }
+        run3(n-k+3);
     }
     return 0;
 }
